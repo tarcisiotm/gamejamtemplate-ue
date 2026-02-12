ef97279614765050de4c8c92f165370c26ece4e6
Tarcisio Costa <tarcisiotm@gmail.com>
2026-02-12 13:44:50 -0800

    wip: level streaming setup

--- Plugins/GameJamTemplate/Source/GameJamTemplate/Private/GJT_LevelManager.cpp
+++ Plugins/GameJamTemplate/Source/GameJamTemplate/Private/GJT_LevelManager.cpp
@@ -2,6 +2,66 @@
 
 #include "GJT_LevelManager.h"
 #include "Kismet/GameplayStatics.h"
+#include "LatentActions.h"
+#include "CoreMinimal.h"
+#include "Engine/LevelStreamingDynamic.h"
+#include "Engine/Engine.h"
+#include "Engine/AssetManager.h"
+#if WITH_EDITOR
+#include "Editor.h"
+#endif
+
+class FGJT_LevelTransitionAction : public FPendingLatentAction
+{
+public:
+    bool* bInternalLoadFinished;
+    FName ExecutionFunction;
+    int32 OutputLink;
+    FWeakObjectPtr CallbackTarget;
+
+    FGJT_LevelTransitionAction(bool* InBool, const FLatentActionInfo& LatentInfo)
+        : bInternalLoadFinished(InBool)
+        , ExecutionFunction(LatentInfo.ExecutionFunction)
+        , OutputLink(LatentInfo.Linkage)
+        , CallbackTarget(LatentInfo.CallbackTarget)
+    {
+    }
+
+    virtual void UpdateOperation(FLatentResponse& Response) override
+    {
+        Response.FinishAndTriggerIf(*bInternalLoadFinished, ExecutionFunction, OutputLink, CallbackTarget);
+    }
+};
+
+void UGJT_LevelManager::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+}
+
+ULevelStreaming* UGJT_LevelManager::GetCurrentLevelStreamingObject(const UObject* WorldContextObject)
+{
+    UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull);
+    if (!World) return nullptr;
+
+    for (ULevelStreaming* Level : World->GetStreamingLevels())
+    {
+        if (Level && Level->IsLevelVisible())
+        {
+            return Level;
+        }
+    }
+    return nullptr;
+}
+
+TSoftObjectPtr<UWorld> UGJT_LevelManager::GetCurrentLevelReference(const UObject* WorldContextObject)
+{
+    ULevelStreaming* LevelObj = GetCurrentLevelStreamingObject(WorldContextObject);
+    if (LevelObj)
+    {
+        return TSoftObjectPtr<UWorld>(LevelObj->GetWorldAsset().ToSoftObjectPath());
+    }
+    return nullptr;
+}
 
 void UGJT_LevelManager::LoadLevelByName(FName LevelName)
 {
@@ -18,7 +78,81 @@ void UGJT_LevelManager::LoadLevelByReference(TSoftObjectPtr<UWorld> LevelRef)
 
     if (!World || LevelRef.IsNull()) { return; }
 
-    // Convert the reference to a string path that OpenLevel understands
     FName LevelName = FName(*LevelRef.GetAssetName());
     UGameplayStatics::OpenLevel(World, LevelName);
+}
+
+void UGJT_LevelManager::StreamLevelAsync(const UObject* WorldContextObject, TSoftObjectPtr<UWorld> LevelRef, FLatentActionInfo LatentInfo)
+{
+    if (LevelRef.IsNull()) return;
+
+    bIsDoneLoading = false;
+
+    UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull);
+    if (!World) return;
+
+    FStreamableManager& Streamable = UAssetManager::GetStreamableManager();
+    Streamable.RequestAsyncLoad(
+        LevelRef.ToSoftObjectPath(),
+        FStreamableDelegate::CreateUObject(this, &UGJT_LevelManager::HandleLoadCompleted, WorldContextObject, LevelRef)
+    );
+
+    // Needed to make the complete node on the BP work without a (new) callback
+    FLatentActionManager& LatentManager = World->GetLatentActionManager();
+    LatentManager.AddNewAction(LatentInfo.CallbackTarget, LatentInfo.UUID,
+        new FGJT_LevelTransitionAction(&bIsDoneLoading, LatentInfo));
+}
+
+void UGJT_LevelManager::HandleLoadCompleted(const UObject* WorldContextObject, TSoftObjectPtr<UWorld> LevelRef)
+{
+    UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull);
+    if (!World || !LevelRef.IsValid()) return;
+
+    for (ULevelStreaming* Streaming : World->GetStreamingLevels())
+    {
+        if (!Streaming) continue;
+
+        if (Streaming->GetWorldAssetPackageName().Contains(LevelRef.GetAssetName()))
+        {
+            Streaming->OnLevelShown.AddDynamic(this, &UGJT_LevelManager::OnLevelShownCallback);
+
+            Streaming->SetShouldBeLoaded(true);
+            Streaming->SetShouldBeVisible(true);
+            World->FlushLevelStreaming(EFlushLevelStreamingType::Full); // spawn actors immediately
+            break;
+        }
+    }
+
+    PreviousLevel = nullptr;
+    LoadingLevel = nullptr;
+    bIsDoneLoading = true;
+}
+
+float UGJT_LevelManager::GetStreamingProgress(TSoftObjectPtr<UWorld> LevelRef)
+{
+    if (LevelRef.IsNull()) return -1.0f;
+
+    ULevelStreaming* Level = UGameplayStatics::GetStreamingLevel(GetWorld(), *LevelRef.GetAssetName());
+    if (Level)
+    {
+        if (Level->IsLevelLoaded()) return 1.0f;
+
+        return Level->IsLevelVisible() ? 1.0f : 0.0f;
+    }
+    return 0.0f;
+}
+
+
+void UGJT_LevelManager::OnLevelShownCallback()
+{
+    UE_LOG(LogTemp, Warning, TEXT("On Level Shown Callback"));
+
+    OnAfterLevelLoad.Broadcast(PreviousLevel, LoadingTargetLevel);
+
+    if (!PreviousLevel.IsNull())
+    {
+        FLatentActionInfo UnloadInfo;
+        UGameplayStatics::UnloadStreamLevel(this, FName(*PreviousLevel.GetAssetName()), UnloadInfo, false);
+    }
+
 }
--- Plugins/GameJamTemplate/Source/GameJamTemplate/Public/GJT_LevelManager.h
+++ Plugins/GameJamTemplate/Source/GameJamTemplate/Public/GJT_LevelManager.h
@@ -6,15 +6,51 @@
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "GJT_LevelManager.generated.h"
 
-UCLASS() // <--- UHT needs this to wake up
-class GAMEJAMTEMPLATE_API UGJT_LevelManager : public UGameInstanceSubsystem // <--- Added inheritance
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FGJTLevelTransitionSignature, TSoftObjectPtr<UWorld>, OldLevel, TSoftObjectPtr<UWorld>, NewLevel);
+
+UCLASS()
+class GAMEJAMTEMPLATE_API UGJT_LevelManager : public UGameInstanceSubsystem
 {
-    GENERATED_BODY() // <--- This connects to the .generated.h file
+    GENERATED_BODY()
+
+protected:
+    TSoftObjectPtr<UWorld> PreviousLevel;
+    TSoftObjectPtr<UWorld> LoadingLevel;
+    UPROPERTY()
+    ULevelStreaming* LoadingLevelInstance;
+    FLatentActionInfo SavedLatentInfo;
+    bool bIsDoneLoading;
 
 public:
+    UPROPERTY(BlueprintAssignable, Category = "GJT | Navigation")
+    FGJTLevelTransitionSignature OnBeforeLevelLoad;
+
+    UPROPERTY(BlueprintAssignable, Category = "GJT | Navigation")
+    FGJTLevelTransitionSignature OnAfterLevelLoad;
+
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+    UFUNCTION(BlueprintPure, Category = "GJT", meta = (WorldContext = "WorldContextObject"))
+    TSoftObjectPtr<UWorld> GetCurrentLevelReference(const UObject* WorldContextObject);
+
+    ULevelStreaming* GetCurrentLevelStreamingObject(const UObject* WorldContextObject);
+
     UFUNCTION(BlueprintCallable, Category = "GJT | Navigation")
     void LoadLevelByName(FName LevelName);
 
     UFUNCTION(BlueprintCallable, Category = "GJT | Navigation")
     void LoadLevelByReference(TSoftObjectPtr<UWorld> LevelRef);
+
+    UFUNCTION(BlueprintCallable, Category = "GJT | Navigation", 
+        meta = (Latent, LatentInfo = "LatentInfo"))
+    void StreamLevelAsync(const UObject* WorldContextObject, TSoftObjectPtr<UWorld> LevelRef, FLatentActionInfo LatentInfo);
+    
+    UFUNCTION(BlueprintCallable)
+    float GetStreamingProgress(TSoftObjectPtr<UWorld> LevelRef);
+
+    UFUNCTION()
+    void HandleLoadCompleted(const UObject* WorldContextObject, TSoftObjectPtr<UWorld> LevelRef);
+
+    UFUNCTION()
+    void OnLevelShownCallback();
 };
